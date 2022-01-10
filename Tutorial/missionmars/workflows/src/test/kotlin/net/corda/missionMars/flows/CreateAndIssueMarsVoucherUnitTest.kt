43f8e2f5da5cebc546a20c6fad1c5edf752cb742
peterli-r3 <peter.li@r3.com>
2022-01-10 21:37:12 +0800

    add notary selection to Unit test

--- Tutorial/missionmars/workflows/src/test/kotlin/net/corda/missionMars/flows/CreateAndIssueMarsVoucherUnitTest.kt
+++ Tutorial/missionmars/workflows/src/test/kotlin/net/corda/missionMars/flows/CreateAndIssueMarsVoucherUnitTest.kt
@@ -17,62 +17,67 @@ import org.junit.jupiter.api.Test
 
 class CreateAndIssueMarsVoucherUnitTest {
 
-//    @Test
-//    fun `flow signs state`() {
-//        flowTest<CreateAndIssueMarsVoucherInitiator> {
-//
-//            // NOTE: this probably should be set up in flowTest
-//            val mockNode = CordaX500Name.parse("O=Mars Express, L=London, C=GB, OU=Space Company")
-//
-//            val inputParams = "{\"voucherDesc\": \"Space Shuttle 323\", \"holder\": \"${mockNode}\"}"
-//            createFlow { CreateAndIssueMarsVoucherInitiator(RpcStartFlowRequestParameters(inputParams)) }
-//
-//            doReturn(mockNode)
-//                .whenever(otherSide)
-//                .name
-//            doReturn(otherSide)
-//                .whenever(flow.identityService)
-//                .partyFromName(mockNode)
-//
-//            doReturn(signedTransactionMock)
-//                .whenever(flow.flowEngine)
-//                .subFlow(any<CollectSignaturesFlow>())
-//
-//            doReturn(signedTransactionMock)
-//                .whenever(flow.flowEngine)
-//                .subFlow(any<FinalityFlow>())
-//
-//            doReturn(
-//                mapOf(
-//                    "voucherDesc" to "Space Shuttle 323",
-//                    "holder" to otherSide.name.toString()
-//                )
-//            )
-//                .whenever(flow.jsonMarshallingService)
-//                .parseJson<Map<String, String>>(inputParams)
-//
-//            flow.call()
-//
-//            // verify notary is set
-//            verify(transactionBuilderMock).setNotary(notary)
-//
-//            // verify the correct output state is created
-//            argumentCaptor<MarsVoucher>().apply {
-//                verify(transactionBuilderMock).addOutputState(capture(), eq(MarsVoucherContract.ID))
-//                assertSoftly {
-//                    it.assertThat(firstValue.issuer).isEqualTo(ourIdentity)
-//                    it.assertThat(firstValue.holder).isEqualTo(otherSide)
-//                    it.assertThat(firstValue.voucherDesc).isEqualTo("Space Shuttle 323")
-//                }
-//            }
-//
-//            // verify command is added
-//            argumentCaptor<Command<CommandData>>().apply {
-//                verify(transactionBuilderMock).addCommand(capture())
-//                assertThat(firstValue.value).isInstanceOf(MarsVoucherContract.Commands.Issue::class.java)
-//                assertThat(firstValue.signers).contains(ourIdentity.owningKey)
-//                assertThat(firstValue.signers).contains(otherSide.owningKey)
-//            }
-//        }
-//    }
+    @Test
+    fun `flow signs state`() {
+        flowTest<CreateAndIssueMarsVoucherInitiator> {
+
+            // NOTE: this probably should be set up in flowTest
+            val mockNode = CordaX500Name.parse("O=Mars Express, L=London, C=GB, OU=Space Company")
+
+            val inputParams = "{\"voucherDesc\": \"Space Shuttle 323\", \"holder\": \"${mockNode}\"}"
+            createFlow { CreateAndIssueMarsVoucherInitiator(RpcStartFlowRequestParameters(inputParams)) }
+
+            doReturn(notary)
+                    .whenever(flow.notaryLookup)
+                    .getNotary(CordaX500Name.parse("O=notary, L=London, C=GB"))
+
+            doReturn(mockNode)
+                .whenever(otherSide)
+                .name
+
+            doReturn(otherSide)
+                .whenever(flow.identityService)
+                .partyFromName(mockNode)
+
+            doReturn(signedTransactionMock)
+                .whenever(flow.flowEngine)
+                .subFlow(any<CollectSignaturesFlow>())
+
+            doReturn(signedTransactionMock)
+                .whenever(flow.flowEngine)
+                .subFlow(any<FinalityFlow>())
+
+            doReturn(
+                mapOf(
+                    "voucherDesc" to "Space Shuttle 323",
+                    "holder" to otherSide.name.toString()
+                )
+            )
+                .whenever(flow.jsonMarshallingService)
+                .parseJson<Map<String, String>>(inputParams)
+
+            flow.call()
+
+            // verify notary is set
+            verify(transactionBuilderMock).setNotary(notary)
+
+            // verify the correct output state is created
+            argumentCaptor<MarsVoucher>().apply {
+                verify(transactionBuilderMock).addOutputState(capture(), eq(MarsVoucherContract.ID))
+                assertSoftly {
+                    it.assertThat(firstValue.issuer).isEqualTo(ourIdentity)
+                    it.assertThat(firstValue.holder).isEqualTo(otherSide)
+                    it.assertThat(firstValue.voucherDesc).isEqualTo("Space Shuttle 323")
+                }
+            }
+
+            // verify command is added
+            argumentCaptor<Command<CommandData>>().apply {
+                verify(transactionBuilderMock).addCommand(capture())
+                assertThat(firstValue.value).isInstanceOf(MarsVoucherContract.Commands.Issue::class.java)
+                assertThat(firstValue.signers).contains(ourIdentity.owningKey)
+                assertThat(firstValue.signers).contains(otherSide.owningKey)
+            }
+        }
+    }
 }
--- Tutorial/missionmars/workflows/src/test/kotlin/net/corda/missionMars/flows/CreateBoardingTicketUnitTest.kt
+++ Tutorial/missionmars/workflows/src/test/kotlin/net/corda/missionMars/flows/CreateBoardingTicketUnitTest.kt
@@ -17,60 +17,64 @@ import org.junit.jupiter.api.Test
 
 class CreateBoardingTicketUnitTest {
 
-//    @Test
-//    fun `flow signs state`() {
-//        flowTest<CreateBoardingTicketInitiator> {
-//
-//            // NOTE: this probably should be set up in flowTest
-//            val mockNode = CordaX500Name.parse("O=Mars Express, L=London, C=GB, OU=Space Company")
-//
-//            val inputParams = "{\"ticketDescription\": \"Space Shuttle 323 - Seat 16B\", \"daysUntilLaunch\": \"10\"}"
-//            createFlow { CreateBoardingTicketInitiator(RpcStartFlowRequestParameters(inputParams)) }
-//
-//            doReturn(mockNode)
-//                    .whenever(otherSide)
-//                    .name
-//
-//            doReturn(signedTransactionMock)
-//                    .whenever(flow.flowEngine)
-//                    .subFlow(any<CollectSignaturesFlow>())
-//
-//            doReturn(signedTransactionMock)
-//                    .whenever(flow.flowEngine)
-//                    .subFlow(any<FinalityFlow>())
-//
-//            doReturn(
-//                    mapOf(
-//                            "ticketDescription" to "Space Shuttle 323 - Seat 16B",
-//                            "daysUntilLaunch" to "10"
-//                    )
-//            )
-//                    .whenever(flow.jsonMarshallingService)
-//                    .parseJson<Map<String, String>>(inputParams)
-//
-//            flow.call()
-//
-//            // verify notary is set
-//            verify(transactionBuilderMock).setNotary(notary)
-//
-//            // verify the correct output state is created
-//            argumentCaptor<BoardingTicket>().apply {
-//                verify(transactionBuilderMock).addOutputState(capture(), eq(BoardingTicketContract.ID))
-//                SoftAssertions.assertSoftly {
-//                    it.assertThat(firstValue.owner).isEqualTo(ourIdentity)
-//                    it.assertThat(firstValue.marsExpress).isEqualTo(ourIdentity)
-//                    it.assertThat(firstValue.description).isEqualTo("Space Shuttle 323 - Seat 16B")
-//                    it.assertThat(firstValue.daysUntilLaunch).isEqualTo(10)
-//
-//                }
-//            }
-//
-//            // verify command is added
-//            argumentCaptor<Command<CommandData>>().apply {
-//                verify(transactionBuilderMock).addCommand(capture())
-//                Assertions.assertThat(firstValue.value).isInstanceOf(BoardingTicketContract.Commands.CreateTicket::class.java)
-//                Assertions.assertThat(firstValue.signers).contains(ourIdentity.owningKey)
-//            }
-//        }
-//    }
+    @Test
+    fun `flow signs state`() {
+        flowTest<CreateBoardingTicketInitiator> {
+
+            // NOTE: this probably should be set up in flowTest
+            val mockNode = CordaX500Name.parse("O=Mars Express, L=London, C=GB, OU=Space Company")
+
+            val inputParams = "{\"ticketDescription\": \"Space Shuttle 323 - Seat 16B\", \"daysUntilLaunch\": \"10\"}"
+            createFlow { CreateBoardingTicketInitiator(RpcStartFlowRequestParameters(inputParams)) }
+
+            doReturn(notary)
+                    .whenever(flow.notaryLookup)
+                    .getNotary(CordaX500Name.parse("O=notary, L=London, C=GB"))
+
+            doReturn(mockNode)
+                    .whenever(otherSide)
+                    .name
+
+            doReturn(signedTransactionMock)
+                    .whenever(flow.flowEngine)
+                    .subFlow(any<CollectSignaturesFlow>())
+
+            doReturn(signedTransactionMock)
+                    .whenever(flow.flowEngine)
+                    .subFlow(any<FinalityFlow>())
+
+            doReturn(
+                    mapOf(
+                            "ticketDescription" to "Space Shuttle 323 - Seat 16B",
+                            "daysUntilLaunch" to "10"
+                    )
+            )
+                    .whenever(flow.jsonMarshallingService)
+                    .parseJson<Map<String, String>>(inputParams)
+
+            flow.call()
+
+            // verify notary is set
+            verify(transactionBuilderMock).setNotary(notary)
+
+            // verify the correct output state is created
+            argumentCaptor<BoardingTicket>().apply {
+                verify(transactionBuilderMock).addOutputState(capture(), eq(BoardingTicketContract.ID))
+                SoftAssertions.assertSoftly {
+                    it.assertThat(firstValue.owner).isEqualTo(ourIdentity)
+                    it.assertThat(firstValue.marsExpress).isEqualTo(ourIdentity)
+                    it.assertThat(firstValue.description).isEqualTo("Space Shuttle 323 - Seat 16B")
+                    it.assertThat(firstValue.daysUntilLaunch).isEqualTo(10)
+
+                }
+            }
+
+            // verify command is added
+            argumentCaptor<Command<CommandData>>().apply {
+                verify(transactionBuilderMock).addCommand(capture())
+                Assertions.assertThat(firstValue.value).isInstanceOf(BoardingTicketContract.Commands.CreateTicket::class.java)
+                Assertions.assertThat(firstValue.signers).contains(ourIdentity.owningKey)
+            }
+        }
+    }
 }
